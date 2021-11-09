2e4839570ed8aa74c4397dbdfda3b8c2e69e23ba
bigdave2058 <92829012+bigdave2058@users.noreply.github.com>
2021-11-09 14:09:33 +1000

    added  test_subtract and test_multiply

--- app/app/test.py
+++ app/app/test.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
-from app.cal import addNumber
+from app.cal import addNumber, subtract,multiply
 
 class CalTests(TestCase):
     def test_addNumbers(self):
         self.assertEqual(addNumber(3,8)11)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(11,3)8)
+
+    def test_multiply(self):
+        self.assertEqual(subtract(11,3)8)
+
+
         
